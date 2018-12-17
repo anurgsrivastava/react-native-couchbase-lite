8e6b5ec289097b6ae9e5f9825750149097d9a53e
Mohammed Anas <anas.mohammed@accoliteindia.com>
2018-12-17 15:07:14 +0530

    Adding configuration classes for Database and Sync Gateway

---
+++ android/src/main/java/com/reactlibrary/SyncGatewayConfig.java
@@ -0,0 +1,81 @@
+package com.reactlibrary;
+
+import com.couchbase.lite.Array;
+import com.couchbase.lite.Replicator;
+import com.couchbase.lite.ReplicatorConfiguration;
+import com.couchbase.lite.SessionAuthenticator;
+import com.couchbase.lite.URLEndpoint;
+import com.facebook.react.bridge.ReadableMap;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * The class represent the Sync Gateway Configuration.
+ * This configuration class will act as a bridge between CBLite and CBServer for
+ * back and forth data syncing.
+ *
+ * @author anas.mohammed
+ */
+public class SyncGatewayConfig {
+
+    //This will be taken from properties files
+    /** websocket endpoint of sync gateway*/
+    private static final String SYNC_GATEWAY_URL = "ws://10.0.2.2:4984/prudential";
+
+    /** Sync Gateway Configuration instance */
+    private static SyncGatewayConfig syncGatewayConfig = null;
+
+    /** Replicator configuration instance which will configured for session management
+     *  channel allocation etc */
+    private static ReplicatorConfiguration replicatorConfiguration;
+
+    /** Replicator instance  responsible for data sync up*/
+    private  static Replicator pushReplicator;
+
+    /**Replicator instance  responsible for data sync down*/
+    private static Replicator pullReplicator;
+
+
+    private SyncGatewayConfig() {
+
+        replicatorConfiguration = replicatorConfiguration();
+    }
+
+    private ReplicatorConfiguration replicatorConfiguration(){
+
+        try {
+            return new ReplicatorConfiguration(DatabaseManager.getDatabase(),
+                    new URLEndpoint(new URI(SYNC_GATEWAY_URL)));
+        } catch (URISyntaxException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the specific replicator based upon the replication type option in readableMap
+     *
+     * @param readableMap holds data that will be used to configure replicatorConfiguration
+     *                    as well as options to get specific replicator.
+     * @return Replicator Instance, either pushReplicator or pullReplicator.
+     */
+    public static Replicator getReplicator(ReadableMap readableMap) {
+        if(syncGatewayConfig == null) {
+            syncGatewayConfig = new SyncGatewayConfig();
+            List<String> channels = new ArrayList<>();
+            channels.add(readableMap.getString("channel"));
+            pushReplicator = new Replicator(replicatorConfiguration
+                    .setAuthenticator(new SessionAuthenticator(readableMap.getString("replicationSessionKey")))
+                    .setReplicatorType(ReplicatorConfiguration.ReplicatorType.PUSH));
+            pullReplicator =  new Replicator(replicatorConfiguration
+                    .setAuthenticator(new SessionAuthenticator(readableMap.getString("replicationSessionKey")))
+                    .setChannels(channels)
+                    .setReplicatorType(ReplicatorConfiguration.ReplicatorType.PULL));
+        }
+        return "PUSH".equalsIgnoreCase(readableMap.getString("replicationType"))?pushReplicator:pullReplicator;
+
+    }
+}
